3419190fc25c1858b6f41f711c28a11ae225b562
Stanislav <S.tanislav-Z.abrodin@yandex.ru>
2022-05-24 21:31:59 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import TimeAttackPage from './Components/TimeAttackPage.jsx';
+import HomePage from './Components/HomePage.jsx';
+import DriftPage from './Components/DriftPage.jsx';
+import ForzaPage from './Components/ForzaPage.jsx';
+import Menu from './Components/Menu.jsx';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Menu />
+      <div className="page">
+        <Routes>
+          <Route path="/drift" element={<DriftPage />} />
+          <Route path="/timeattack" element={<TimeAttackPage />} />
+          <Route path="/forza" element={<ForzaPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
-
-export default App;
